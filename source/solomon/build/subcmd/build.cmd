8b56f483afa9fafa0d980fe4d249e74741c2b27c
道化師 <wraith@trickpalace.net>
2016-01-27 10:11:31 +0900

    solomon の導入

---
+++ source/solomon/build/subcmd/build.cmd
@@ -0,0 +1,36 @@
+@REM
+@REM build
+@REM
+
+
+@REM
+@REM 準備
+@REM
+
+@CALL VERSION.cmd
+
+
+@REM
+@REM version.json の作成
+@REM
+
+@ECHO "%VERSION_MAJOR%.%VERSION_MINOR%.%VERSION_BUILD%" >web\version.json
+
+
+@REM
+@REM 本体処理
+@REM
+
+@REM コンパイル結果のコピー先ディレクトリの作成
+@SET RESULT_DIR=%SOLOMON_SNAPSHOT_DIR%\result\%BUILD_NAME%
+@IF NOT EXIST "%RESULT_DIR%" CALL "%SOLOMON_LIBCMD_DIR%\mkdir.ex.cmd" "%RESULT_DIR%"
+
+@REM コンパイル結果ディレクトリにバージョン情報ファイルが存在していたら削除
+@IF EXIST "%RESULT_DIR%\VERSION.cmd" DEL "%RESULT_DIR%\VERSION.cmd"
+
+@REM コンパイル結果をコピー
+@CALL "%SOLOMON_MAKE_SNAPSHOT_CMD%" ".\web" "%RESULT_DIR%\web" >NUL
+@COPY /Y ".\VERSION.cmd" "%RESULT_DIR%" >NUL
+
+@CALL "%SOLOMON_COMPILE_SUCCESS_CMD%"
+@CALL "%SOLOMON_TEST_SUCCESS_CMD%"
