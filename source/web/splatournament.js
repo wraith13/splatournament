ea4a0ece9a3a8fdd9688d9a6fa7312851ca929e5
道化師 <wraith@trickpalace.net>
2016-02-15 23:33:53 +0900

    match オブジェクトに型情報が入ってなかった問題の修正

--- source/web/splatournament.js
+++ source/web/splatournament.js
@@ -48,189 +48,6 @@ angular.module('angularUUID2', []).factory('uuid2', [
 //    document.getElementById("picker").appendChild(pickerButton);
 //};
 
-var margin = { top: 30, right: 10, bottom: 10, left: 10 },
-    //width = screen.width - margin.left - margin.right,
-    width = 2700 - margin.left - margin.right,
-    halfWidth = width / 2,
-    height = 5000 - margin.top - margin.bottom,
-    i = 0,
-    duration = 500,
-    root;
-
-var getChildren = function (d) {
-    var a = [];
-    if (d.entries) for (var i = 0; i < d.entries.length; i++) {
-        d.entries[i].isRight = false;
-        d.entries[i].parent = d;
-        a.push(d.entries[i]);
-    }
-    return a.length ? a : null;
-};
-
-var tree = d3.layout.tree()
-    .size([height, width]);
-
-var diagonal = d3.svg.diagonal()
-    .projection(function (d) { return [d.y, d.x]; });
-var elbow = function (d, i) {
-    var source = calcLeft(d.source);
-    var target = calcLeft(d.target);
-    var hy = (target.y - source.y) / 2;
-    if (d.isRight) hy = -hy;
-    return "M" + source.y + "," + source.x
-            + "H" + (source.y + hy)
-            + "V" + target.x + "H" + target.y;
-};
-var connector = elbow;
-
-var calcLeft = function (d) {
-    var l = d.y;
-    if (!d.isRight) {
-        l = d.y - halfWidth;
-        l = halfWidth - l;
-    }
-    return { x: d.x, y: l };
-};
-
-var vis = d3.select("#chart").append("svg")
-    .attr("width", width + margin.right + margin.left)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-var update_tournament_tree = function (json, showObject) {
-    root = json;
-    root.x0 = height / 2;
-    root.y0 = width / 2;
-
-    var t1 = d3.layout.tree().size([height, halfWidth]).children(function (d) { return d.entries; });
-    t1.nodes(root);
-
-    var rebuildChildren = function (node) {
-        node.children = getChildren(node);
-        if (node.children) node.children.forEach(rebuildChildren);
-    }
-    rebuildChildren(root);
-    root.isRight = false;
-    update(root, showObject);
-}
-
-var toArray = function (item, arr) {
-    arr = arr || [];
-    var i = 0, l = item.children ? item.children.length : 0;
-    arr.push(item);
-    for (; i < l; i++) {
-        toArray(item.children[i], arr);
-    }
-    return arr;
-};
-
-function update(source, showObject) {
-    // Compute the new tree layout.
-    var nodes = toArray(source);
-
-    // Normalize for fixed-depth.
-    nodes.forEach(function (d) { d.y = d.depth * 180 + halfWidth; });
-
-    // Update the nodes…
-    var node = vis.selectAll("g.node")
-        .data(nodes, function (d) { return d.id || (d.id = ++i); });
-
-    // Enter any new nodes at the parent's previous position.
-    var nodeEnter = node.enter().append("g")
-        .attr("class", function (d) {
-            return (d.name && 0 < d.name.length) ? "node" : "null";
-        })
-        .attr("transform", function (d) { return "translate(" + source.y0 + "," + source.x0 + ")"; });
-        //.on("click", click);
-
-    nodeEnter.append("circle")
-        .attr("r", 1e-6)
-        .style("fill", function (d) { return d._children ? "lightsteelblue" : "#fff"; });
-
-    nodeEnter.append("text")
-        .attr("dy", function (d) { return d.isRight ? 14 : -8; })
-        .attr("text-anchor", "middle")
-        .on("click", function (d) {
-            if (d.name) { showObject(d.original_id) }
-        })
-        .text(function (d) { return d.name; })
-        .style("fill-opacity", 1e-6);
-
-    // Transition nodes to their new position.
-    var nodeUpdate = node.transition()
-        .duration(duration)
-        .attr("transform", function (d) { p = calcLeft(d); return "translate(" + p.y + "," + p.x + ")"; });
-
-    nodeUpdate.select("circle")
-        .attr("r", 4.5)
-        .style("fill", function (d) { return d._children ? "lightsteelblue" : "#fff"; });
-
-    nodeUpdate.select("text")
-        .style("fill-opacity", 1);
-
-    // Transition exiting nodes to the parent's new position.
-    var nodeExit = node.exit().transition()
-        .duration(duration)
-        .attr("transform", function (d) { p = calcLeft(d.parent || source); return "translate(" + p.y + "," + p.x + ")"; })
-        .remove();
-
-    nodeExit.select("circle")
-        .attr("r", 1e-6);
-
-    nodeExit.select("text")
-        .style("fill-opacity", 1e-6);
-
-    // Update the links...
-    var link = vis.selectAll("path.link")
-        .data(tree.links(nodes), function (d) { return d.target.id; });
-
-    // Enter any new links at the parent's previous position.
-    link.enter().insert("path", "g")
-        .attr("class", function (d) {
-            return (d.target.is_winner || (d.source.is_winner && 1 == d.source.entries.length)) ? "link winner" : "link";
-        })
-        .attr("d", function (d) {
-            var o = { x: source.x0, y: source.y0 };
-            return connector({ source: o, target: o });
-        });
-
-    // Transition links to their new position.
-    link.transition()
-        .duration(duration)
-        .attr("d", connector);
-
-    // Transition exiting nodes to the parent's new position.
-    link.exit().transition()
-        .duration(duration)
-        .attr("d", function (d) {
-            var o = calcLeft(d.source || source);
-            if (d.source.isRight) o.y -= halfWidth - (d.target.y - d.source.y);
-            else o.y += halfWidth - (d.target.y - d.source.y);
-            return connector({ source: o, target: o });
-        })
-        .remove();
-    // Stash the old positions for transition.
-    nodes.forEach(function (d) {
-        var p = calcLeft(d);
-        d.x0 = p.x;
-        d.y0 = p.y;
-    });
-
-    // Toggle children on click.
-    //function click(d) {
-    //    if (d.children) {
-    //        d._children = d.children;
-    //        d.children = null;
-    //    } else {
-    //        d.children = d._children;
-    //        d._children = null;
-    //    }
-    //    update(source, showObject);
-    //}
-}
-//d3.json("bracket.json", update_tournament_tree);
-
 var app = angular.module("splatournament", ['ui.router', "ui.bootstrap", "angularUUID2"]);
 
 app.config(["$locationProvider", function ($locationProvider) {
@@ -641,7 +458,7 @@ app.controller("splatornament", function ($rootScope, $scope, $http, $location, 
         var entries_count = $scope.model.entries.length;
         var unmatched_match = [];
         var addMatch = function (entries) {
-            var match = {};
+            var match = { type:"match" };
             match.entries = entries;
             $scope.model.matches.push(match);
             unmatched_match.push(match);
@@ -837,6 +654,193 @@ app.controller("splatornament", function ($rootScope, $scope, $http, $location, 
             return result;
         };
         if ($scope.model.matches && 0 < $scope.model.matches.length) {
+            var height_count = $scope.model.entries.length - ($scope.unmatches || []).length;
+            var width_count = $scope.model.matches[$scope.model.matches.length -1].level +1;
+
+            var margin = { top: 30, right: 10, bottom: 10, left: 10 },
+                //width = screen.width - margin.left - margin.right,
+                width = (width_count * 180) - margin.left - margin.right,
+                //halfWidth = width / 2,
+                halfWidth = width,
+                height = (height_count * 60) - margin.top - margin.bottom,
+                i = 0,
+                duration = 500,
+                root;
+
+            var getChildren = function (d) {
+                var a = [];
+                if (d.entries) for (var i = 0; i < d.entries.length; i++) {
+                    d.entries[i].isRight = false;
+                    d.entries[i].parent = d;
+                    a.push(d.entries[i]);
+                }
+                return a.length ? a : null;
+            };
+
+            var tree = d3.layout.tree()
+                .size([height, width]);
+
+            var diagonal = d3.svg.diagonal()
+                .projection(function (d) { return [d.y, d.x]; });
+            var elbow = function (d, i) {
+                var source = calcLeft(d.source);
+                var target = calcLeft(d.target);
+                var hy = (target.y - source.y) / 2;
+                if (d.isRight) hy = -hy;
+                return "M" + source.y + "," + source.x
+                        + "H" + (source.y + hy)
+                        + "V" + target.x + "H" + target.y;
+            };
+            var connector = elbow;
+
+            var calcLeft = function (d) {
+                var l = d.y;
+                if (!d.isRight) {
+                    l = d.y - halfWidth;
+                    l = halfWidth - l;
+                }
+                return { x: d.x, y: l };
+            };
+
+            var vis = d3.select("#chart").append("svg")
+                .attr("width", width + margin.right + margin.left)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+            var update_tournament_tree = function (json, showObject) {
+                root = json;
+                root.x0 = height / 2;
+                root.y0 = width / 2;
+
+                var t1 = d3.layout.tree().size([height, halfWidth]).children(function (d) { return d.entries; });
+                t1.nodes(root);
+
+                var rebuildChildren = function (node) {
+                    node.children = getChildren(node);
+                    if (node.children) node.children.forEach(rebuildChildren);
+                }
+                rebuildChildren(root);
+                root.isRight = false;
+                update(root, showObject);
+            }
+
+            var toArray = function (item, arr) {
+                arr = arr || [];
+                var i = 0, l = item.children ? item.children.length : 0;
+                arr.push(item);
+                for (; i < l; i++) {
+                    toArray(item.children[i], arr);
+                }
+                return arr;
+            };
+
+            function update(source, showObject) {
+                // Compute the new tree layout.
+                var nodes = toArray(source);
+
+                // Normalize for fixed-depth.
+                nodes.forEach(function (d) { d.y = d.depth * 180 + halfWidth; });
+
+                // Update the nodes…
+                var node = vis.selectAll("g.node")
+                    .data(nodes, function (d) { return d.id || (d.id = ++i); });
+
+                // Enter any new nodes at the parent's previous position.
+                var nodeEnter = node.enter().append("g")
+                    .attr("class", function (d) {
+                        return (d.name && 0 < d.name.length) ? "node" : "null";
+                    })
+                    .attr("transform", function (d) { return "translate(" + source.y0 + "," + source.x0 + ")"; });
+                //.on("click", click);
+
+                nodeEnter.append("circle")
+                    .attr("r", 1e-6)
+                    .style("fill", function (d) { return d._children ? "lightsteelblue" : "#fff"; });
+
+                nodeEnter.append("text")
+                    .attr("dy", function (d) { return d.isRight ? 14 : -8; })
+                    .attr("text-anchor", "middle")
+                    .on("click", function (d) {
+                        if (d.name) { showObject(d.original_id) }
+                    })
+                    .text(function (d) { return d.name; })
+                    .style("fill-opacity", 1e-6);
+
+                // Transition nodes to their new position.
+                var nodeUpdate = node.transition()
+                    .duration(duration)
+                    .attr("transform", function (d) { p = calcLeft(d); return "translate(" + p.y + "," + p.x + ")"; });
+
+                nodeUpdate.select("circle")
+                    .attr("r", 4.5)
+                    .style("fill", function (d) { return d._children ? "lightsteelblue" : "#fff"; });
+
+                nodeUpdate.select("text")
+                    .style("fill-opacity", 1);
+
+                // Transition exiting nodes to the parent's new position.
+                var nodeExit = node.exit().transition()
+                    .duration(duration)
+                    .attr("transform", function (d) { p = calcLeft(d.parent || source); return "translate(" + p.y + "," + p.x + ")"; })
+                    .remove();
+
+                nodeExit.select("circle")
+                    .attr("r", 1e-6);
+
+                nodeExit.select("text")
+                    .style("fill-opacity", 1e-6);
+
+                // Update the links...
+                var link = vis.selectAll("path.link")
+                    .data(tree.links(nodes), function (d) { return d.target.id; });
+
+                // Enter any new links at the parent's previous position.
+                link.enter().insert("path", "g")
+                    .attr("class", function (d) {
+                        return (d.target.is_winner || (d.source.is_winner && 1 == d.source.entries.length)) ? "link winner" : "link";
+                    })
+                    .attr("d", function (d) {
+                        var o = { x: source.x0, y: source.y0 };
+                        return connector({ source: o, target: o });
+                    });
+
+                // Transition links to their new position.
+                link.transition()
+                    .duration(duration)
+                    .attr("d", connector);
+
+                // Transition exiting nodes to the parent's new position.
+                link.exit().transition()
+                    .duration(duration)
+                    .attr("d", function (d) {
+                        var o = calcLeft(d.source || source);
+                        if (d.source.isRight) o.y -= halfWidth - (d.target.y - d.source.y);
+                        else o.y += halfWidth - (d.target.y - d.source.y);
+                        return connector({ source: o, target: o });
+                    })
+                    .remove();
+                // Stash the old positions for transition.
+                nodes.forEach(function (d) {
+                    var p = calcLeft(d);
+                    d.x0 = p.x;
+                    d.y0 = p.y;
+                });
+
+                // Toggle children on click.
+                //function click(d) {
+                //    if (d.children) {
+                //        d._children = d.children;
+                //        d.children = null;
+                //    } else {
+                //        d.children = d._children;
+                //        d._children = null;
+                //    }
+                //    update(source, showObject);
+                //}
+            }
+            //d3.json("bracket.json", update_tournament_tree);
+
             update_tournament_tree(match_to_tree($scope.model.matches[$scope.model.matches.length -1]), $scope.showObject);
         }
     };
